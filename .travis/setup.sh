79d28993b915cea1e850d95bbc4c6fba60f22494
Chandan Kumar <chandanchaudhary@outlook.com>
2020-02-12 22:38:12 +0530

    Change aws environment variables

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 # Region Setup
-aws configure set default.region ${REGION}
+aws configure set default.region ${AWS_DEFAULT_REGION}
 
 # Docker Login to ECR
 $(aws ecr get-login --no-include-email)
